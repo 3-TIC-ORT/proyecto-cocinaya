55b31977f78c4f8902a05d9d227faea72d97bb01
Your Name <you@example.com>
2025-09-03 10:42:32 -0300

    haciendo las estrellas

---
+++ Front/Recetas/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let estrellas = document.querySelectorAll(".estrellas span");
+  
+    estrellas.forEach(estrella => {
+      estrella.addEventListener("click", () => {
+        let valor = parseInt(estrella.getAttribute("data-value"));
+  
+        
+        estrellas.forEach(e => e.classList.remove("selected"));
+  
+       
+        for (let i = 0; i < valor; i++) {
+          estrellas[i].classList.add("selected");
+        }
+      });
+    });
+  });
+
+  
+  
