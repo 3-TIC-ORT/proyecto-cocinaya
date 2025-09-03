c7ba5e0798ce409d23379cc72e961e53d37c3ad6
Lorenzo Saenz <you@example.com>
2025-09-03 15:51:45 -0300

    CASICASICSICASI FINAL INICIO DE SECION

--- Back/INICIODESESION/Creacion.js
+++ Back/INICIODESESION/Creacion.js
@@ -1,6 +1,6 @@
 import fs from "fs"
-let nuevonombrer= lolo 
-let nuevacontraseña= 1234
+let nuevonombrer= "lolo"
+let nuevacontraseña= "1234"
 let basededatosinter = fs.readFileSync ("INICIODESECION/Usuarios.json", "utf-8");
 let basededatos = JSON.parse (basededatosinter)
 
@@ -8,6 +8,7 @@ let basededatos = JSON.parse (basededatosinter)
 
 function registrarse(nuevonombre, nuevacontraseña){
 let registro = {nombre : nuevonombre, contraseña: nuevacontraseña}
+return registro;
 }
 basededatos.push (registrarse)
 
@@ -18,6 +19,8 @@ fs.writeFileSync ("Usuarios.json", JSON.stringify (basededatos, null, 2))
 function iniciodesesion (nombredeusuario, contraseña) {
 for  (let i = 0 ; i< basededatos.length ; i++)
 {
-if (basededatos[i].nombre ===  completar){}
+if (basededatos[i].nombre ===  nombredeusuario && basededatos[i].contraseña === contraseña){
+return true;
+}
 }
 }
