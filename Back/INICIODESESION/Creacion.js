54ef7ebd444fad594352df5968706cd713b4fbaa
Lorenzo Saenz <you@example.com>
2025-09-05 12:09:31 -0300

    REGISTRARSE FINAL SIN SOQUETIC

--- Back/INICIODESESION/Creacion.js
+++ Back/INICIODESESION/Creacion.js
@@ -1,26 +1,26 @@
 import fs from "fs"
-let nuevonombrer= "lolo"
-let nuevacontraseña= "1234"
-let basededatosinter = fs.readFileSync ("INICIODESECION/Usuarios.json", "utf-8");
-let basededatos = JSON.parse (basededatosinter)
-
+let nuevonombre= "shuli" // nombre traido de front
+let nuevacontraseña= "mejorprofe" // contra traida de front
+let basededatosinter = fs.readFileSync ("./Usuarios.json", "utf-8");
+let basededatos = JSON.parse(basededatosinter)
 
 
 function registrarse(nuevonombre, nuevacontraseña){
-let registro = {nombre : nuevonombre, contraseña: nuevacontraseña}
+let registro = {nombre :  nuevonombre, contraseña: nuevacontraseña}
 return registro;
+
 }
-basededatos.push (registrarse)
+basededatos.push (registrarse(nuevonombre, nuevacontraseña))
 
-fs.writeFileSync ("Usuarios.json", JSON.stringify (basededatos, null, 2))
-
+fs.writeFileSync ("./Usuarios.json", JSON.stringify (basededatos, null, 2))
 
 
 function iniciodesesion (nombredeusuario, contraseña) {
 for  (let i = 0 ; i< basededatos.length ; i++)
 {
 if (basededatos[i].nombre ===  nombredeusuario && basededatos[i].contraseña === contraseña){
-return true;
+return console.log ("contraseña correcta")//ingreso a la pagina;
 }
+else return console.log ("contraseña o usuario incorrectos")
 }
 }
