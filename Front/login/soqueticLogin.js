535232bbebba646dd4518ae47bfccd0515f0aec9
Your Name <you@example.com>
2025-10-29 10:45:55 -0300

    soqueticLogin

---
+++ Front/login/soqueticLogin.js
@@ -0,0 +1,17 @@
+connect2Server();
+
+const usuario = document.getElementById("usuario");
+const contraseña = document.getElementById("contraseña");
+const boton = document.getElementById("boton");
+
+if (boton) {
+  boton.addEventListener("click", () => {
+    const usuarioFinal = usuario.value;
+    const contraseñaFinal = contraseña.value;
+
+    postEvent("login", { user: usuarioFinal, contraseña: contraseñaFinal }, (respuesta) => {
+      console.log("Respuesta del backend:", respuesta);
+      alert(respuesta);
+    });
+  });
+}
