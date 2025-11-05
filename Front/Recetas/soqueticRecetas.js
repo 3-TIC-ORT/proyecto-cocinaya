9151c176379cd11a6b74365372a8a6e2340ded36
Su Nombre <urisasson15@gmail.com>
2025-11-05 15:52:35 -0300

    sigo

--- Front/Recetas/soqueticRecetas.js
+++ Front/Recetas/soqueticRecetas.js
@@ -2,7 +2,7 @@ connect2Server();
 
 const contenedor = document.getElementById("contenedorRecetas");
 
-getEvent("leerrecetas", {}, (recetas) => {
+getEvent("leerrecetas", (recetas) => {
   console.log("Recetas recibidas:", recetas);
 
   contenedor.innerHTML = ""; 
@@ -12,21 +12,6 @@ getEvent("leerrecetas", {}, (recetas) => {
     card.className = "tarjetaReceta";
 
     
-    const pasos = receta.procedimiento.split("|").map(p => `<li>${p.trim()}</li>`).join("");
-
-    card.innerHTML = `
-      <img src="${receta.imagen}" style="width:200px;border-radius:10px;">
-      <h2>${receta.nombre}</h2>
-      
-      <p><b>Ingredientes:</b></p>
-      <ul>
-        ${receta.ingredientes.map(i => `<li>${i.cantidad} de ${i.tipo}</li>`).join("")}
-      </ul>
-
-      <p><b>Procedimiento:</b></p>
-      <ol>${pasos}</ol>
-    `;
-
-    contenedor.appendChild(card);
+    
   });
 });
