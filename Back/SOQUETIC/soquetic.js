98e8a2a70762c30f8f1b80dba7d77f399ead7334
Lorenzo Saenz <you@example.com>
2025-11-07 15:10:47 -0300

    PASAMOS TODO A UN ARCHIVO

--- Back/SOQUETIC/soquetic.js
+++ Back/SOQUETIC/soquetic.js
@@ -4,8 +4,26 @@ import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } fro
 
 import { iniciodesesion } from "../INICIODESESION/Creacion.js"
 import { registroEvent } from "../INICIODESESION/Creacion.js"
+import {agregarFavorito} from "../PANTALLA/recetas.js"
+import {favoritos} from "../PANTALLA/recetas.js"
+import {valoracion} from "../PANTALLA/recetas.js"
+import {leerrecetas} from "../PANTALLA/recetas.js"
+
 
 subscribePOSTEvent ("registro",registroEvent )
 subscribePOSTEvent ("iniciodesesion", iniciodesesion)
 console.log ("soquetic anda")
+
+
+subscribePOSTEvent("agregarFavorito", agregarFavorito);
+subscribeGETEvent("favoritos", favoritos);
+subscribePOSTEvent("agregarValoracion", valoracion);
+subscribeGETEvent ("leerrecetas", leerrecetas)
+
+
+
 startServer (3000, true)
+
+
+
+
