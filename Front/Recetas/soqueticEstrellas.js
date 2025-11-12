5bab39a1c9d414e6f944401911036e68b81b7cde
Su Nombre <urisasson15@gmail.com>
2025-11-12 12:05:24 -0300

    css

--- Front/Recetas/soqueticEstrellas.js
+++ Front/Recetas/soqueticEstrellas.js
@@ -5,16 +5,31 @@ const cont = document.querySelector(".contenido");
 
 if (receta && cont) {
   const infoHTML = `
+
+  <div class="receta">
+  <div class="imagen">
+  <img src="${receta.imagen}" alt="${receta.nombre}" style="width:300px;border-radius:10px;">
+  </div>
+  
+  <div class="detalles">
     <h2 id="nombre">${receta.nombre}</h2>
-    <img src="${receta.imagen}" alt="${receta.nombre}" style="width:300px;border-radius:10px;">
+    
     <h4>Ingredientes</h4>
     <ul id="ingredientes">
       ${receta.ingredientes.map(i => `<li>${i.cantidad} ${i.tipo}</li>`).join("")}
     </ul>
     <h4>Procedimiento</h4>
-    <ol id="procedimiento">${receta.procedimiento}</ol>
+    <ol id="procedimiento">${receta.procedimiento
+      .split(".")
+      .filter(p => p.trim() !== "")
+      .map(p => `<li>${p.trim()}</li>`)
+      .join("") 
+    }</ol>
+    </div>
+    
     <br>
-    <button id="btnFav">Agregar a Favoritos ⭐</button>
+    <button id="btnFav"><img src="../imagenes/estrella.png" alt="estrella" style="width:30px;"></button>
+    
   `;
   cont.insertAdjacentHTML("beforeend", infoHTML);
 
